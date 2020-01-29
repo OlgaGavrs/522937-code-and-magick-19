927e8ed4f698596b12f9f627727ea20c2115b74f
OlgaGavrs <labellezza1989@gmail.com>
2020-01-29 09:23:26 +0400

    Устранила замечания

--- js/setup.js
+++ js/setup.js
@@ -2,47 +2,52 @@
 var COUNT_WIZARD = 4;
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
 var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var secondNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
 
 var getRandomIndex = function (maxIndex) {
   return Math.floor(Math.random() * maxIndex);
 };
 
-var names = [];
-for (var i = 0; i < COUNT_WIZARD; i++) {
-  names[i] = firstNames[getRandomIndex(firstNames.length)] + ' ' + secondNames[getRandomIndex(secondNames.length)];
-}
+var getArrayNames = function (arr1, arr2) {
+  var arr = [];
+  for (var i = 0; i < COUNT_WIZARD; i++) {
+    arr.push(arr1[getRandomIndex(arr1.length)] + ' ' + arr2[getRandomIndex(arr2.length)]);
+  }
+  return arr;
+};
 
-var wizards = names.map(function (val) {
+var getWizard = function (val) {
   return {
     name: val,
     coatColor: coatColors[getRandomIndex(coatColors.length)],
     eyesColor: eyesColors[getRandomIndex(eyesColors.length)]
   };
-});
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+};
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
+
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
+userDialog.classList.remove('hidden');
 
-wizards.forEach(function (obj, index) {
-  fragment.appendChild(renderWizard(wizards[index]));
+var wizards = getArrayNames(firstNames, secondNames).map(getWizard);
+
+wizards.forEach(function (wizard) {
+  fragment.appendChild(renderWizard(wizard));
 });
 
 similarListElement.appendChild(fragment);
--- js/stat.js
+++ js/stat.js
@@ -8,9 +8,11 @@ var GAP = 20;
 var TEXT_HEIGHT = 16;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
+var BAR_WIDTH_GAP = BAR_WIDTH + BAR_GAP;
 var MAX_BAR_HEIGHT = 150;
 var YOU = 'Вы';
 var COLOR_TEXT = '#000';
+var YOUR_COLOR_BAR = 'rgba(255, 0, 0, 1)';
 
 var renderRect = function (ctx, x1, y1, x2, y2, color) {
   ctx.fillStyle = color;
@@ -24,6 +26,12 @@ var outputText = function (ctx, font, baseline, style, text, x, y) {
   ctx.fillText(text, x, y);
 };
 
+var getColorBar = function () {
+  var Saturation = Math.round(Math.random() * 100);
+  var color = 'hsl(240, ' + Saturation + '%, 50%)';
+  return color;
+};
+
 window.renderStatistics = function (ctx, names, times) {
   renderRect(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
   renderRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#fff');
@@ -31,30 +39,25 @@ window.renderStatistics = function (ctx, names, times) {
   outputText(ctx, TEXT_HEIGHT + 'px PT Mono', 'hanging', COLOR_TEXT, 'Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP);
   outputText(ctx, TEXT_HEIGHT + 'px PT Mono', 'hanging', COLOR_TEXT, 'Список результатов:', CLOUD_X + GAP, CLOUD_Y + 2 * GAP);
 
-  var values = names.map(function (val, i) {
+  var scores = names.map(function (val, i) {
     return {name: val, time: times[i]};
   }).sort(function (a, b) {
     return b.time - a.time;
   });
 
-  var maxTime = values[0].time;
+  var maxTime = scores[0].time;
 
   var renderBar = function (i) {
-    var time = Math.floor(values[i].time);
-    var BAR_HEIGHT = (MAX_BAR_HEIGHT * values[i].time) / maxTime;
-    var BAR_WIDTH_GAP = BAR_WIDTH + BAR_GAP;
-    var BAR_Y = CLOUD_Y + CLOUD_HEIGHT - GAP - TEXT_HEIGHT - GAP / 2 - BAR_HEIGHT;
+    var time = Math.floor(scores[i].time);
+    var barHeight = (MAX_BAR_HEIGHT * scores[i].time) / maxTime;
+    var BAR_Y = CLOUD_Y + CLOUD_HEIGHT - GAP - TEXT_HEIGHT - GAP / 2 - barHeight;
     var colorBar;
 
-    outputText(ctx, TEXT_HEIGHT + 'px PT Mono', 'alphabetic', COLOR_TEXT, values[i].name, CLOUD_X + 2 * GAP + BAR_WIDTH_GAP * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
+    outputText(ctx, TEXT_HEIGHT + 'px PT Mono', 'alphabetic', COLOR_TEXT, scores[i].name, CLOUD_X + 2 * GAP + BAR_WIDTH_GAP * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
 
-    if (values[i].name === YOU) {
-      colorBar = 'rgba(255, 0, 0, 1)';
-    } else {
-      var Saturation = Math.round(Math.random() * 100);
-      colorBar = 'hsl(240, ' + Saturation + '%, 50%)';
-    }
-    renderRect(ctx, CLOUD_X + 2 * GAP + BAR_WIDTH_GAP * i, BAR_Y, BAR_WIDTH, BAR_HEIGHT, colorBar);
+    colorBar = scores[i].name === YOU ? YOUR_COLOR_BAR : getColorBar();
+
+    renderRect(ctx, CLOUD_X + 2 * GAP + BAR_WIDTH_GAP * i, BAR_Y, BAR_WIDTH, barHeight, colorBar);
 
     outputText(ctx, TEXT_HEIGHT + 'px PT Mono', 'alphabetic', COLOR_TEXT, time, CLOUD_X + 2 * GAP + BAR_WIDTH_GAP * i, BAR_Y - GAP / 2);
   };

ed721858b09d051df4ee827ac95c9d6259f4c94f
OlgaGavrs <labellezza1989@gmail.com>
2020-03-02 13:01:46 +0400

    Создала модуль backend.js и поправила код программы

--- js/colorize.js
+++ js/colorize.js
@@ -4,20 +4,32 @@
     return arrColors[Math.floor(Math.random() * arrColors.length)];
   };
 
-  window.colorize = {
-    coloring: function (element, colors, inputName) {
-      element.addEventListener('click', function () {
-        var color = getRandomColor(colors);
-        if (element.tagName.toLowerCase() === 'div') {
-          element.style.backgroundColor = color;
-        } else {
-          element.style.fill = color;
-        }
-        document.querySelector('[name = ' + inputName + ']').value = color;
-      });
-    },
-    color: function (colors) {
-      return getRandomColor(colors);
-    }
+  window.colorize = function (element, colors, inputName) {
+    element.addEventListener('click', function () {
+      var color = getRandomColor(colors);
+      if (element.tagName.toLowerCase() === 'div') {
+        element.style.backgroundColor = color;
+      } else {
+        element.style.fill = color;
+      }
+      document.querySelector('[name = ' + inputName + ']').value = color;
+    });
   };
+
+  // window.colorize = {
+  //   coloring: function (element, colors, inputName) {
+  //     element.addEventListener('click', function () {
+  //       var color = getRandomColor(colors);
+  //       if (element.tagName.toLowerCase() === 'div') {
+  //         element.style.backgroundColor = color;
+  //       } else {
+  //         element.style.fill = color;
+  //       }
+  //       document.querySelector('[name = ' + inputName + ']').value = color;
+  //     });
+  //   },
+  //   color: function (colors) {
+  //     return getRandomColor(colors);
+  //   }
+  // };
 })();
